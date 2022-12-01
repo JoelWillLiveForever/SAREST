cfc2d167b2eccf4e9402b6bb26c1244ed449490f
Vladimir Nikulin <mail.jorey@gmail.com>
2022-12-01 18:21:38 +0300

    А вареник то сочным оказался

--- app/src/main/java/com/example/sarest/MainActivity.java
+++ app/src/main/java/com/example/sarest/MainActivity.java
@@ -15,10 +15,10 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.example.sarest.API.models.*;
@@ -30,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
     private CardsAdapter mCardsAdapter;
     private RequestQueue mRequestQueue;
 
-    private ArrayList<Criterion> mCriterionList;
-    private ArrayList<Grade> mGradeList;
-    private ArrayList<Group> mGroupList;
-    private ArrayList<Subject> mSubjectList;
-    private ArrayList<Subject_Group> mSubjectGroupList;
-    private ArrayList<Teacher> mTeacherList;
-    private ArrayList<Teacher_Subject> mTeacherSubjectList;
-    private ArrayList<User> mUserList;
+    private List<Criterion> mCriterionList;
+    private List<Grade> mGradeList;
+    private List<Group> mGroupList;
+    private List<Subject> mSubjectList;
+    private List<Subject_Group> mSubjectGroupList;
+    private List<Teacher> mTeacherList;
+    private List<Teacher_Subject> mTeacherSubjectList;
+    private List<User> mUserList;
 
     private ArrayList<Card> mCardList;
 
@@ -47,84 +47,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = findViewById(R.id.recycler_view);
-//        mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
-
-        mUserList = new ArrayList<User>();
-        mTeacherList = new ArrayList<Teacher>();
-        mSubjectList = new ArrayList<Subject>();
-        mTeacherSubjectList = new ArrayList<Teacher_Subject>();
-        mSubjectGroupList = new ArrayList<Subject_Group>();
-        mCriterionList = new ArrayList<Criterion>();
-        mGradeList = new ArrayList<Grade>();
-        mGroupList = new ArrayList<Group>();
-
-        mCardList = new ArrayList<>();
 
         parseJSON();
-
-        // тут будем собирать карточки
-        getCards();
-
-        mCardsAdapter = new CardsAdapter(MainActivity.this, mCardList);
-        mRecyclerView.setAdapter(mCardsAdapter);
-
-        // Output in a logcat //
-//        for (User u: mUserList)
-//        {
-//            Log.d(LOG_TAG, "USER: id = " + u.id +
-//                    "email = " + u.email +
-//                    "surname = " + u.surname +
-//                    "name = " + u.name +
-//                    "lastname = " + u.lastname);
-//        }
-//        for (Teacher t: mTeacherList)
-//        {
-//            Log.d(LOG_TAG, "TEACHER: user = " + t.user);
-//        }
-//        for (Subject s: mSubjectList)
-//        {
-//            Log.d(LOG_TAG, "SUBJECT: id = " + s.id +
-//                    "name = " + s.name);
-//        }
-//        for (Teacher_Subject ts: mTeacherSubjectList)
-//        {
-//            Log.d(LOG_TAG, "TEACHER_SUBJECT: id = " + ts.id +
-//                    "teacher = " + ts.teacherId +
-//                    "subject = " + ts.subjectId);
-//        }
-//        for (Subject_Group sg: mSubjectGroupList)
-//        {
-//            Log.d(LOG_TAG, "SUBJECT_GROUP: id = " + sg.id +
-//                    "subject = " + sg.subjectId +
-//                    "group = " + sg.groupId);
-//        }
-//        for (Criterion s: mCriterionList)
-//        {
-//            Log.d(LOG_TAG, "Criterion: id = " + s.id +
-//                    "name = " + s.name);
-//        }
-//        for (Grade g: mGradeList)
-//        {
-//            Log.d(LOG_TAG, "GRADE: id = " + g.id +
-//                    "teacher_subject = " + g.teacher_subjectId +
-//                    "criterion = " + g.criterionId +
-//                    "grade = " + g.grade);
-//        }
-//        for (Group gp: mGroupList)
-//        {
-//            Log.d(LOG_TAG, "Group: id = " + gp.id +
-//                    "name = " + gp.name);
-//        }
-        // Output in a logcat //
-
-        for (Card my_card : mCardList)
-        {
-            Log.i(LOG_TAG, "CARD: FIO = " + my_card.getFIO() +
-                    "subject = " + my_card.getSubject() +
-                    "score = " + my_card.getScore());
-        }
     }
 
     private void parseJSON() {
@@ -139,81 +64,131 @@ public class MainActivity extends AppCompatActivity {
                 "group_list"
         };
 
-        for (String url : urls) {
-            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "http://10.0.2.2:8000/api/v1/" + url, null,
+        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+        for (String url: urls) {
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, "http://10.0.2.2:8000/api/v1/" + url, null,
                     new Response.Listener<JSONArray>() {
                         @Override
                         public void onResponse(JSONArray response) {
-                            try {
-                                for (int j = 0; j < response.length(); j++) {
-                                    JSONObject json = response.getJSONObject(j);
+                            if (mUserList == null)
+                                mUserList = new ArrayList<>();
+                            if (mTeacherList == null)
+                                mTeacherList = new ArrayList<>();
+                            if (mSubjectList == null)
+                                mSubjectList = new ArrayList<>();
+                            if (mTeacherSubjectList == null)
+                                mTeacherSubjectList = new ArrayList<>();
+                            if (mSubjectGroupList == null)
+                                mSubjectGroupList = new ArrayList<>();
+                            if (mCriterionList == null)
+                                mCriterionList = new ArrayList<>();
+                            if (mGradeList == null)
+                                mGradeList = new ArrayList<>();
+                            if (mGroupList == null)
+                                mGroupList = new ArrayList<>();
+                            if (mCardList == null)
+                                mCardList = new ArrayList<>();
 
-                                    switch (url) {
-                                        case "user_list":
+                            try {
+                                switch (url) {
+                                    case "user_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int userId = json.getInt("id");
                                             String email = json.getString("email");
                                             String surname = json.getString("surname");
                                             String name = json.getString("name");
                                             String lastname = json.getString("lastname");
+
                                             mUserList.add(new User(userId, email, surname, name, lastname));
-                                            break;
-                                        case "teacher_list":
+                                        }
+                                        break;
+                                    case "teacher_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int user = json.getInt("user");
 
                                             mTeacherList.add(new Teacher(user));
-                                            break;
-                                        case "subject_list":
+                                        }
+                                        break;
+                                    case "subject_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int subjId = json.getInt("id");
                                             String subjName = json.getString("name");
 
                                             mSubjectList.add(new Subject(subjId, subjName));
-                                            break;
-                                        case "teacher_subj_list":
+                                        }
+                                        break;
+                                    case "teacher_subj_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int teacherSubjId = json.getInt("id");
                                             int TSteacherId = json.getInt("teacher");
                                             int TSsubjectId = json.getInt("subject");
+
                                             mTeacherSubjectList.add(new Teacher_Subject(teacherSubjId, TSteacherId, TSsubjectId));
-                                            break;
-                                        case "subject_group_list":
+                                        }
+                                        break;
+                                    case "subject_group_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
                                             int subjGroupId = json.getInt("id");
                                             int SGsubjectId = json.getInt("subject");
                                             int SGgroupId = json.getInt("group");
+
                                             mSubjectGroupList.add(new Subject_Group(subjGroupId, SGsubjectId, SGgroupId));
-                                            break;
-                                        case "criterion_list":
+                                        }
+                                        break;
+                                    case "criterion_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int critId = json.getInt("id");
                                             String critName = json.getString("name");
 
                                             mCriterionList.add(new Criterion(critId, critName));
-                                            break;
-                                        case "grade_list":
+                                        }
+                                        break;
+                                    case "grade_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int gradeId = json.getInt("id");
                                             int gradeTSId = json.getInt("teacher_subject");
                                             int gradeCriterionId = json.getInt("criterion");
                                             int grade = json.getInt("grade");
 
                                             mGradeList.add(new Grade(gradeId, gradeTSId, gradeCriterionId, grade));
-                                            break;
-                                        case "group_list":
+                                        }
+                                        break;
+                                    case "group_list":
+                                        for (int i = 0; i < response.length(); i++) {
+                                            JSONObject json = response.getJSONObject(i);
+
                                             int groupId = json.getInt("id");
                                             String groupName = json.getString("name");
 
                                             mGroupList.add(new Group(groupId, groupName));
-                                            break;
-                                        default:
-                                            // error
-                                            throw new Exception("Invalid API request");
-                                    }
+                                        }
 
-                                    Log.i(LOG_TAG, json.toString());
+                                        // это последний запрос
+                                        getCards();
+
+                                        mCardsAdapter = new CardsAdapter(MainActivity.this, mCardList);
+                                        mRecyclerView.setAdapter(mCardsAdapter);
+
+                                        break;
+                                    default:
+                                        // error
+                                        throw new Exception("Invalid API request");
                                 }
-
-                                //                             mExampleAdapter = new ExampleAdapter(MainActivity.this, mExampleList);
-                                //                             Log.d(LOG_TAG, "ADAPTER = " + (mExampleAdapter == null ? "NULL" : "NOT NULL"));
-                                //
-                                //                             mRecyclerView.setAdapter(mExampleAdapter);
-                                //                             Log.d(LOG_TAG, "RECYCLER ADAPTER = " + (mRecyclerView.getAdapter() == null ? "NULL" : "NOT NULL"));
                             } catch (Exception e) {
+                                Log.e(LOG_TAG, e.getMessage());
                                 e.printStackTrace();
                             }
                         }
@@ -221,59 +196,54 @@ public class MainActivity extends AppCompatActivity {
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            error.printStackTrace();
+                            Log.e(LOG_TAG, error.getMessage());
                         }
                     });
-            mRequestQueue.add(request);
+            mRequestQueue.add(jsonArrayRequest);
         }
     }
 
     protected void getCards() {
         // ФИО препода, дисциплина и рейтинг
 
-        for (int i = 0; i < 10; i++)
-        {
-            mCardList.add(new Card("Заебало", "Сука blyat", i));
+        for (Subject_Group sg : mSubjectGroupList) {
+            for (Teacher_Subject ts : mTeacherSubjectList)
+            {
+                if (ts.subjectId == sg.subjectId)
+                {
+                    for (Criterion crit : mCriterionList)
+                    {
+                        for (Grade grade : mGradeList)
+                        {
+                            if (grade.teacher_subjectId == ts.id && grade.criterionId == crit.id)
+                            {
+                                for (User user : mUserList)
+                                {
+                                    if (ts.teacherId == user.id)
+                                    {
+                                        for(Subject s : mSubjectList)
+                                        {
+                                            if (s.id == ts.subjectId)
+                                            {
+                                                String FIO;
+                                                if (!Objects.equals(user.lastname, ""))
+                                                {
+                                                    FIO = user.surname + user.name + user.lastname;
+                                                }
+                                                else
+                                                {
+                                                    FIO = user.surname + user.name;
+                                                }
+                                                mCardList.add(new Card(FIO, s.name, grade.grade));
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-//        for (Subject_Group sg : mSubjectGroupList) {
-//            for (Teacher_Subject ts : mTeacherSubjectList)
-//            {
-//                if (ts.subjectId == sg.subjectId)
-//                {
-//                    for (Criterion crit : mCriterionList)
-//                    {
-//                        for (Grade grade : mGradeList)
-//                        {
-//                            if (grade.teacher_subjectId == ts.id && grade.criterionId == crit.id)
-//                            {
-//                                for (User user : mUserList)
-//                                {
-//                                    if (ts.teacherId == user.id)
-//                                    {
-//                                        for(Subject s : mSubjectList)
-//                                        {
-//                                            if (s.id == ts.subjectId)
-//                                            {
-//                                                String FIO;
-//                                                if (!Objects.equals(user.lastname, ""))
-//                                                {
-//                                                    FIO = user.surname + user.name + user.lastname;
-//                                                }
-//                                                else
-//                                                {
-//                                                    FIO = user.surname + user.name;
-//                                                }
-//                                                mCardList.add(new Card(FIO, s.name, grade.grade));
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
     }
 }
